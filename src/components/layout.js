83efdbf0c8a45516b8c27d974cfa37308d8a8332
jbrock1981 <jbrock1981@gmail.com>
2019-02-27 06:43:22 -0600

    .

--- src/components/layout.js
+++ src/components/layout.js
@@ -48,7 +48,7 @@ const TemplateWrapper = ({ children }) => (
       <div className="container__sidebar">
         <div className="sidebar">
           <h6 className="sidebar__title">
-            <Link to="/">Lambda Labs Blog</Link>
+            <Link to="/">Lambda Labs Blog - by jbrock</Link>
           </h6>
           <div
             className="sidebar__intro"
